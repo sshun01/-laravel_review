d96b8c4f6c8833b6f58af5c9d45ef434e41b24c1
sshun <shun909@gmail.com>
2017-05-02 04:45:05 +0000

    Generate Edit page and deine update method

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('app')
+
+@section('content')
+ <h1>Edit:{!! $article->title !!}</h1>
+
+  {!! Form::model($article, ['method' => 'PATCH', 'action' => ['ArticlesController@update', $article->id]]) !!}
+    <div class="form-group">
+      {!! Form::label('title', 'Title:') !!}
+      {!! Form::text('title', null, ['class' => 'form-control']) !!}
+   </div>
+  
+   <div class="form-group">
+      {!! Form::label('body', 'Body:') !!}
+      {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+   </div>
+
+   <div class="form-group">
+      {!! Form::label('published_at', 'Publish On:') !!}
+      {!! Form::input('date', 'published_at', date('Y-m-d'),  ['class' => 'form-control']) !!}
+   </div>
+
+@stop
